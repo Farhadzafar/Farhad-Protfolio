6b921074afa883b14aee77335dba3f72cf48308b
farhadzafar <farhadzafari10@gmail.com>
2024-11-01 03:07:08 +0430

    change the tailwind config filll

--- src/components/sections/Resume.jsx
+++ src/components/sections/Resume.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import Section from "../Section";
 
 export default function Resume() {
+  // const [item, setItem] = useState("skills");
+  const classes = `w-full cursor-pointer border-b-[3px]  hover:border-color-3/60 hover:text-gray-500 py-4`;
   return (
-    <Section has="resume" color="bg-red-50">
-      Resume
+    <Section has="resume" color="bg-slate-50" classes="py-24">
+      <span className=" span">3+ years of experience</span>
+      <h3 className=" h3 mb-8">My Resume</h3>
+
+      <ul className="w-full grid grid-cols-4 justify-center content-center text-center font-medium ">
+        <li className={`${classes}`}>Eduction</li>
+        <li className={` ${classes} border-color-3 `}>Skill</li>
+        <li className={`${classes}`}>Internship </li>
+        <li className={`${classes}`}>srtipcat</li>
+      </ul>
     </Section>
   );
 }
